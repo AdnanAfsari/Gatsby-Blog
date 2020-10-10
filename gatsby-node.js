dd82427c7e1b6d4ebbed61c529ac2e4e8c213679
Adnan Afsari <adnanafsari.swe@gmail.com>
2020-10-10 17:04:38 +0600

    gatsby-node.js setup to dynamically creat page for each blog post

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,45 @@
  */
 
 // You can delete this file if you're not using it
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+const path = require(`path`)
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const BlogPostTemplate = path.resolve("./src/templates/BlogPost.js")
+
+  const result = await graphql(`
+    {
+      allWordpressPost {
+        edges {
+          node {
+            slug
+            wordpress_id
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  const BlogPosts = result.data.allWordpressPost.edges
+  BlogPosts.forEach(post => {
+    createPage({
+      path: `/post/${post.node.slug}`,
+      component: BlogPostTemplate,
+      context: {
+        id: post.node.wordpress_id,
+      },
+    })
+  })
+}
