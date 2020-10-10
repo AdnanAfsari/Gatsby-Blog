fb875511016ede3e6178cc91339e10174325af3d
Adnan Afsari <adnanafsari.swe@gmail.com>
2020-10-10 16:39:05 +0600

    path package add

---
+++ src/templates/BlogPost.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const BlogPostTemplate = () => (
+  <Layout>
+    <h1>Blog Post Template</h1>
+  </Layout>
+)
+
+export default BlogPostTemplate
